5d0e8ccf097ef23c07bc65a44e94d8e4407b15da
mdrasel97 <mollarasel972@gmail.com>
2025-11-20 09:56:10 +0600

    conditional type added

---
+++ conditional.ts
@@ -0,0 +1,17 @@
+// conditional type: 
+
+type A = null
+type B = undefined
+
+type C = A extends null ? true: false;
+
+
+type RichPeopleVehicle = {
+    bike: string,
+    car: string,
+    ship: string
+}
+
+type CheckVehicle<T> = T extends keyof RichPeopleVehicle ? true : false
+
+type HasBike = CheckVehicle<'tractor'>
